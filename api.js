12637b8560b692ef182ff9b6da1e951af3a1ca7e
Callum Thompson <callumrthompson@me.com>
2019-04-15 17:39:16 +1000

    created basic get request

--- api.js
+++ api.js
@@ -2,11 +2,23 @@ const axios = require("axios")
 const express = require("express")
 
 const app = express()
-
-const apiKeys = []
-
+app.use(express.json());
 
 const port = 3000
 app.listen(port, console.log(`Listening on port ${port}...`))
 
-// axios.get()
+const apiKeys = [] // 5 x string constants
+
+// API Key is rate limited to 5 weather reports an hour. After that your service should respond in a way which communicates that the hourly limit has been exceeded.
+
+// Have a URL that accepts both a city name and country name
+
+app.get('/', (req, res) => {
+  res.status(200).send("GET request received!")
+})
+
+// Based upon these inputs, and the API Key, your service should decide whether or not to call the OpenWeatherMap name service.
+
+// If it does, the only weather data you need to return to the client is the description field from the weather JSON result
+
+// Reject requests with invalid input or missing API Keys.
