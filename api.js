9bae5ba135b20f7c3149b3a620785f5a9ed2e90a
Callum Thompson <callumrthompson@me.com>
2019-04-19 12:55:18 +1000

    deleted request package, implemented correct data call, implemented http status and message response on GET request

--- api.js
+++ api.js
@@ -1,26 +1,66 @@
-const axios = require("axios")
+// const request = require('request')
+const axios = require('axios')
 const express = require("express")
 
 const app = express()
-app.use(express.json());
+
+app.use(express.json())
 
 const port = 3000
 app.listen(port, console.log(`Listening on port ${port}...`))
 
-const apiKeys = [] // 5 x string constants
+// 5 x string constants (may need to seperate these into seperate arrays, one with key and the other with timestamp and apiKey index)
 
-// API Key is rate limited to 5 weather reports an hour. After that your service should respond in a way which communicates that the hourly limit has been exceeded.
+const apiKeys = [
+  {apiKey: "apikey1", lastUsed: null},
+  {apiKey: "apikey2", lastUsed: null},
+  {apiKey: "apikey3", lastUsed: null},
+  {apiKey: "apikey4", lastUsed: null},
+  {apiKey: "apikey4", lastUsed: null}
+]
 
-// Have a URL that accepts both a city name and country name
+// Check API Key (requires middleware)
+
+const checkAPIKey = () => {
+  
+}
+
+
+// Retrieve Weather
+const retrieveWeather = (city, country) => {
+  return new Promise((resolve, reject) => {
+    const apiKey = '788fd636d2797af705dd3d71c77f89d5'
+    const weatherURL = 'http://api.openweathermap.org/data/2.5/weather?q='
+    const requestURL = `${weatherURL}${city},${country}&appid=${apiKey}`
+    axios.get(requestURL)
+      .then(response => {
+        const status = response.status
+        const weatherDescription = `Your weather report: ${response.data.weather[0].description}`
+        const data = {
+          status: status,
+          message: weatherDescription
+        }
+        resolve(data)
+      })
+      .catch(response => {
+        const errorStatus = response.response.data.cod
+        const errorMessage = `Error: ${response.response.data.message}`
+        const data = {
+          status: errorStatus,
+          message: errorMessage  
+        }
+        reject(data)
+      })
+    })
+}
+
+// Perform GET request
 
 app.get('/:params', (req, res) => {
-  res.status(200).send("GET request received!")
-  console.log("Params:", req.params)
-  console.log(req.headers)
+  const params = req.params.params.split("&")
+  const city = params[0].split("=")[1]
+  const country = params[1].split("=")[1]
+  retrieveWeather(city, country)
+    .then((data) => res.status(data.status).send(data.message))
+    .catch((data) => res.status(data.status).send(data.message))
 })
-
-// Based upon these inputs, and the API Key, your service should decide whether or not to call the OpenWeatherMap name service.
-
-// If it does, the only weather data you need to return to the client is the description field from the weather JSON result
-
-// Reject requests with invalid input or missing API Keys.
